5db936283839124a3dd6c6485258d0e085f14306
Claudia Saxer <Claudia.Saxer@zuehlke.com>
2023-03-21 13:35:44 +0100

    add cosmos db

--- ServerlessT4/CreateRating.cs
+++ ServerlessT4/CreateRating.cs
@@ -23,24 +23,30 @@ namespace Z.CreateRating
         }
 
         [FunctionName("CreateRating")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "userId", "productId", "locationName", "rating", "userNotes" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiParameter(name: "userId", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **userId** parameter")]
+        [OpenApiParameter(name: "productId", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **productId** parameter")]
+        [OpenApiParameter(name: "locationName", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **locationName** parameter")]
+        [OpenApiParameter(name: "rating", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **rating** parameter")]
+        [OpenApiParameter(name: "userNotes", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **userNotes** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var userId = data?.userId;
+            var productId = data?.productId;
+            var locationName = data?.locationName;
+            var rating = data?.rating;
+            var userNotes = data?.userNotes;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            string responseMessage = string.IsNullOrEmpty(userId)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {userId}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
