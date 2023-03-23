6af8a5a66f4dce1c119f244f4d86b3a5075fe43e
Claudia Saxer <Claudia.Saxer@zuehlke.com>
2023-03-23 15:39:55 +0100

    refactor

--- ServerlessT4/CreateRating.cs
+++ ServerlessT4/CreateRating.cs
@@ -107,5 +107,14 @@ namespace Z.CreateRating
         [JsonPropertyName("productDescription")]
         public string ProductDescription { get; set; }
     }
+
+    internal class CreateRatingDto
+    {
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public string LocationName { get; set; }
+        public int Rating { get; set; }
+        public string UserNotes { get; set; }
+    }
 }
 
